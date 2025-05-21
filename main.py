f53d4598340e8744f7d97f165327b075e85e0589
Olonov Aleksandr <adrolv08111985@gmail.com>
2025-05-21 22:05:21 +0300

    Инициализация проекта

---
+++ main.py
@@ -0,0 +1,7 @@
+# 8185492514:AAFk9oJ-t0_gabUTdTH1vt2Hp2xRHmEGysU
+from telegram import WebAppInfo, InlineKeyboardButton
+
+button = InlineKeyboardButton(
+    text="Открыть To-Do",
+    web_app=WebAppInfo(url="https://yourdomain.com/todo")
+)
